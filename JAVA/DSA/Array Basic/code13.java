3b82dd83bd99ee2853943589793dff143ca51088
nishantcg13 <galandesonu42@gmail.com>
2024-08-08 23:29:48 +0530

    08 Aug

---
+++ JAVA/DSA/Array Basic/code13.java
@@ -0,0 +1,48 @@
+/*
+ * 
+ * 13] Find unique element
+
+Given an array of size n which contains all elements occurring in multiples of K,
+except one element which doesn't occur in multiple of K. Find that unique element.
+Example 1:
+Input :
+n = 7, k = 3
+arr[] = {6, 2, 5, 2, 2, 6, 6}
+Output :
+5
+Explanation:
+Every element appears 3 times except 5.
+Example 2:
+Input :
+n = 5, k = 4
+arr[] = {2, 2, 2, 10, 2}
+Output :
+10
+Explanation:
+Every element appears 4 times except 10.
+Expected Time Complexity: O(N. Log(A[i]) )
+Expected Auxiliary Space: O( Log(A[i]) )
+Constraints:
+3<= N<=2*10^5
+2<= K<=2*10^5
+1<= A[i]<=10^9
+
+ */
+
+class unique{
+    static void uniqueEle(){
+        Scanner sc = new Scanner (System.in);
+        System.err.println("Enter the size of the array: ");
+        int size = sc.nextInt();
+        System.out.println("Enter the array Elements: ");
+        int arr[] = new int[size];
+        for(int i =0 ; i<size; i++){
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+
+        for(int i=0;i<size;i++){
+            
+        }
+    }
+}
